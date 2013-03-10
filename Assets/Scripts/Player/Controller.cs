ddae1f5ff43b6c0e723b04993756374aa49a4c41
jaakaappi <diapaasi@gmail.com>
2013-03-10 18:05:15 +0200

    Extended the base file hierarchy, started UI

---
+++ Assets/Scripts/Player/Controller.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+
+public class Controller : MonoBehaviour
+{
+	public int health;
+	
+	void Start ()
+	{
+	
+	}
+	
+	void Update ()
+	{
+	
+	}
+}
---
+++ Assets/Scripts/UI/UI.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+
+//The base UI-script that controls the player HUD, menus and so on.
+public class UI : MonoBehaviour
+{
+
+	void Start ()
+	{
+	
+	}
+	
+
+	void Update ()
+	{
+	
+	}
+	
+	void OnGUI()
+	{
+		
+	}
+}
